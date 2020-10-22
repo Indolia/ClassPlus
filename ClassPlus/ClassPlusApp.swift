ece2e81e0b4d2edab9975f8d34d5f1a6cab496dd
Rishi pal <rishipal.indolia@jungleworks.com>
2020-10-22 22:13:45 +0530

    Initial Commit

---
+++ ClassPlus/ClassPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassPlusApp.swift
+//  ClassPlus
+//
+//  Created by Rishi pal on 22/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassPlus
+//
+//  Created by Rishi pal on 22/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
